e09b7e540f73c2e0a3cdccb6fc79c08cdf9977df
System Administrator <root@mini-de-alan.home>
2021-08-10 21:48:08 -0300

    Meu primeiro commit

---
+++ src/pages/aula01/index.js
@@ -0,0 +1,79 @@
+const meuPrimeiroArray =
+    [
+        { nome: 'alan', idade: 33, url: 'www.nome.com/imagem' },
+        { nome: 'Daniel', idade: 20, url: 'www.nome.com/imagem' },
+        { nome: 'Gael', idade: 1, url: 'www.nome.com/imagem' },
+        { nome: 'Fabio', idade: 20, url: 'www.nome.com/imagem' },
+        { nome: 'Martins', idade: 22, url: 'www.nome.com/imagem' }
+    ]
+// console.log(meuPrimeiroArray);
+
+// for (let i = 0; i < meuPrimeiroArray.length ; i++) {
+//     console.log(meuPrimeiroArray[i]);
+// }
+
+//const res = meuPrimeiroArray.map((el) => { console.log(meuPrimeiroArray)})
+
+// const res = meuPrimeiroArray.map((el) => {
+//     console.log(el.idade * 2)
+// })
+
+//filter
+
+//reducer 
+
+
+// let array1 = ['branco', 10, 20, 30, null];
+
+// let array2 = ['verde', 40, 50, 60, undefined];
+
+// array1 = array1.concat(array2);
+
+// console.log(array1);
+
+// var array1 = ['branco', 10, 20, 30];
+
+// array1 = array1.join('-');
+
+// console.log(array1);
+
+// const  array1 = ['branco', 10, 20, 30];
+
+// array1.push('novo elemento');
+// array1.push('novo 2');
+// array1.push('novo 3');
+
+// console.log(array1);
+
+// const array1 = ['branco', 10, 20, 30, "novo elemento"];
+
+// array1.pop();
+// array1.pop();
+
+// console.log(array1);
+
+// const array1 = ['branco', 10, 20, 30, "novo elemento"];
+
+// array1.shift();
+
+// console.log(array1);
+
+
+// const array1 = ['branco', 10, 20, 30, "novo elemento"];
+// const  novoArray = array1.slice(1,3).join('---');
+// console.log(novoArray);
+
+// const array1 = ['branco', 10, 20, 30, "novo elemento"];
+
+// array1.splice(1,2, {});
+
+// console.log(array1);
+
+const  array1 = ['branco', 10, 20, 30, "novo elemento"];
+
+array1.reverse();
+
+console.log(array1);
+
+
+
